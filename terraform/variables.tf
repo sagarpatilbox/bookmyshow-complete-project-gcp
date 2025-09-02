8a784f26f67a2bd1dd0d1191966bcbfa46fe886f
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-02 22:56:11 +0530

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "project_id" { type = string }
-variable "region" { type = string default = "asia-south1" }
-variable "app_image" { type = string }
-variable "db_password" { type = string sensitive = true }
-variable "service_name" { type = string default = "bookmyshow-service" }
