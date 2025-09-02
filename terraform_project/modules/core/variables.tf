d175e23f65653189e8139e03de92d99ee9c2a19e
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 01:33:45 +0530

    Delete terraform_project directory

--- terraform_project/modules/core/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "network" { type = string }
-variable "db_password" { type = string }
-variable "app_image" { type = string }
--- terraform_project/modules/mig_lb/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "network" { type = string }
-variable "app_image" { type = string }
