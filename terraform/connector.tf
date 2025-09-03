2d6aac84078a64cea492d9ec05fdd8519923e346
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 11:56:59 +0530

    Update connector.tf

--- terraform/connector.tf
+++ terraform/connector.tf
@@ -1,6 +1,10 @@
 resource "google_vpc_access_connector" "connector" {
-  name          = "bookmyshow-connector"
-  region        = var.region
-  network       = google_compute_network.vpc.id
-  ip_cidr_range = "10.8.0.0/28"
+  name    = "bookmyshow-connector"
+  region  = var.region
+  network = google_compute_network.vpc.name
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
