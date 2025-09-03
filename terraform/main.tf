8f7c0cae224961301944c53aefd7d92804d18354
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 13:38:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,31 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+resource "google_cloud_run_service" "bookmyshow" {
+  name     = "bookmyshow-service"
+  location = "asia-south1"
+
+  template {
+    spec {
+      containers {
+        # Use latest image pushed to Artifact Registry
+        image = "asia-south1-docker.pkg.dev/${var.project_id}/bookmyshow-repo/bookmyshow:${var.commit_sha}"
+        resources {
+          limits = {
+            cpu    = "1"
+            memory = "512Mi"
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+output "cloud_run_service_url" {
+  value = google_cloud_run_service.bookmyshow.status[0].url
+}
