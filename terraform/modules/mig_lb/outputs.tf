932b56196c6594ca7d1d5bca7f1b6baf543570b5
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 11:40:19 +0530

    Delete terraform/modules directory

--- terraform/modules/mig_lb/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "mig_name" {
-  value = google_compute_region_instance_group_manager.mig.name
-}
-
-output "lb_ip" {
-  value = google_compute_global_address.default.address
-}
--- terraform/modules/mig_lb/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "network" { type = string }
-variable "app_image" { type = string }
-variable "zone" {
-  description = "Zone for MIG and autoscaler"
-  type        = string
-}
