3d1757377f0aff47d280eda68163d59f7a737bfc
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 11:59:43 +0530

    Update bucket.tf

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,5 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name          = "${var.project_id}-bucket"
-  location      = var.region
-  force_destroy = var.force_destroy_bucket
+  name     = "${var.project_id}-bucket"
+  location = var.region
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
