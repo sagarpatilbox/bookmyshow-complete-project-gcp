7676c67e61350df57d3cdef12392aba3d54452ce
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 13:50:50 +0530

    Update artifact.tf

--- terraform/artifact.tf
+++ terraform/artifact.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "repo" {
   location      = var.region
-  repository_id = "bookmyshow"
+  repository_id = "bookmyshow-repo"   # ✅ match manual repo
   format        = "DOCKER"
 
   lifecycle {
