0bc92c0ca82102b8171064d1fe5f974ed290d02c
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 11:59:08 +0530

    Update artifact.tf

--- terraform/artifact.tf
+++ terraform/artifact.tf
@@ -2,4 +2,9 @@ resource "google_artifact_registry_repository" "repo" {
   location      = var.region
   repository_id = "bookmyshow"
   format        = "DOCKER"
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
