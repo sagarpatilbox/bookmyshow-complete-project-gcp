96c53e66ca46dae5a677d60fd0830ae216204a29
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 11:57:22 +0530

    Update redis.tf

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -3,4 +3,9 @@ resource "google_redis_instance" "cache" {
   tier           = "BASIC"
   memory_size_gb = 1
   region         = var.region
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
