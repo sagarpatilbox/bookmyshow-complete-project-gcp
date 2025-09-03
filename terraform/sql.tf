67487a49ab299b4f6707713c8eec41c8e967e9b7
sagarpatilbox <sagarpatilbox@gmail.com>
2025-09-03 11:49:06 +0530

    Update sql.tf

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,8 +1,3 @@
-data "google_secret_manager_secret_version" "db_password" {
-  secret  = "db-password"
-  project = var.project_id
-}
-
 resource "google_sql_database_instance" "db" {
   name             = "bookmyshow-db"
   database_version = "POSTGRES_14"
@@ -17,5 +12,5 @@ resource "google_sql_database_instance" "db" {
   }
 
   deletion_protection = false
-  root_password       = data.google_secret_manager_secret_version.db_password.secret_data
+  root_password       = "MySecurePass123"
 }
